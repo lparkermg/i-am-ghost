a04c244edcd4443ffb8afbfeafbbbe2b478d4cac
Luke Parker <lparkermg@outlook.com>
2021-11-23 21:12:10 +0000

    Adds file creation to AddStep.

--- src/IAmGhost.Tests/JsonFileGhostServiceTests.cs
+++ src/IAmGhost.Tests/JsonFileGhostServiceTests.cs
@@ -1,18 +1,55 @@
-using NUnit.Framework;
 using IAmGhost.Services;
+using NUnit.Framework;
+using System;
+using System.IO;
 
 namespace IAmGhost.Tests;
 
 public class JsonFileGhostServiceTests
 {
-    [SetUp]
-    public void Setup()
+    private readonly string _basePath = "./TestBase/";
+
+    [TearDown]
+    public void TearDown()
     {
+        if (Directory.Exists(_basePath)) 
+        { 
+            Directory.Delete(_basePath, true);
+        }
     }
 
     [Test]
     public void AddStep_GivenEmptyGuid_ThrowsArgumentException()
     {
-        Assert.Pass();
+        var service = new JsonFileGhostService(string.Empty);
+        Assert.That(() => service.AddStep(Guid.Empty), Throws.ArgumentException.With.Message.EqualTo("Id must not be empty"));
     }
+
+    [Test]
+    public void AddStep_GivenGuid_SavesJsonFileWithIdAtProvidedPath()
+    {
+        var basePath = _basePath;
+        var id = Guid.NewGuid();
+
+        var service = new JsonFileGhostService(basePath);
+
+        service.AddStep(id);
+
+        var path = Path.Combine(basePath, $"{id}.json");
+        Assert.That(File.Exists(path), Is.True);
+    }
+
+    /*
+    Finish tomorrow.
+    [Test]
+    public void AddTest_GivenGhostDataWithNewFile_CorrectlyPopulatesFile()
+    {
+        var id = Guid.NewGuid();
+        var stepData = "Step Data, Loc 1, Loc 2";
+        var service = new JsonFileGhostService(_basePath);
+
+        service.AddStep(id, stepData);
+
+
+    }*/
 }
--- src/IAmGhost/Services/JsonFileGhostService.cs
+++ src/IAmGhost/Services/JsonFileGhostService.cs
@@ -1,11 +1,33 @@
+// <copyright file="JsonFileGhostService.cs" company="Luke Parker">
+// Copyright (c) Luke Parker. All rights reserved.
+// </copyright>
+
 using IAmGhost.Interfaces;
 
 namespace IAmGhost.Services;
 
 public class JsonFileGhostService : IGhostService
 {
+    private readonly string _basePath;
+
+    public JsonFileGhostService(string basePath) => _basePath = basePath;
+
     public Task<int> AddStep(Guid ghostId)
     {
-        throw new NotImplementedException();
+        if (ghostId == Guid.Empty)
+        {
+            throw new ArgumentException("Id must not be empty");
+        }
+
+        if (!Directory.Exists(_basePath))
+        {
+            Directory.CreateDirectory(_basePath);
+        }
+
+        var path = Path.Combine(_basePath, $"{ghostId}.json");
+        using (var file = File.Create(path))
+        {
+        }
+        return Task.FromResult(0);
     }
 }
